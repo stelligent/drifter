512f8c97e935a55e42fd4ae141655d41a353d3b9
Casey Lee <casey.lee@stelligent.com>
2018-04-17 20:49:47 +0000

    debug

--- toolchain/check-drift.sh
+++ toolchain/check-drift.sh
@@ -12,10 +12,12 @@ STACK_NAME=$1
 
 ### Initiate drift detection
 DRIFT_DETECTION_ID=$(aws cfn detect-stack-drift --stack-name ${STACK_NAME} --query StackDriftDetectionId --output text)
+echo ${DRIFT_DETECTION_ID}
 
 ### Wait for detection to complete
 echo -n "Waiting for drift detection to complete..."
 while true; do
+    aws cfn describe-stack-drift-detection-status --stack-drift-detection-id ${DRIFT_DETECTION_ID} 
     DETECTION_STATUS=$(aws cfn describe-stack-drift-detection-status --stack-drift-detection-id ${DRIFT_DETECTION_ID} --query DetectionStatus --output text) 
     if [ "DETECTION_IN_PROGRESS" = "${DETECTION_STATUS}" ]; then 
         echo -n "."
@@ -34,3 +36,5 @@ if [ "DRIFTED" = "${STACK_DRIFT_STATUS}" ]; then
         --query 'StackResourceDrifts[?StackResourceDriftStatus!=`IN_SYNC`].{Type:ResourceType, Resource:LogicalResourceId, Status:StackResourceDriftStatus, Diff:PropertyDifferences}' >&2 
     exit 1 
 fi
+
+aws cfn describe-stack-resource-drifts --stack-name ${STACK_NAME} 
