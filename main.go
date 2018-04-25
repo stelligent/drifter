5cd585dac38dbefed6c7f8e1b77e965e07c3e6ba
Casey Lee <casey.lee@stelligent.com>
2018-04-25 04:38:40 +0000

    add feature flag

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"os"
+	"strconv"
 )
 
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	featureFlag, err := strconv.ParseBool(os.Getenv("FEATURE_FLAG"))
+
 	return events.APIGatewayProxyResponse{
-		Body:       "Hello world!",
+		Body: fmt.Sprintf("Hello world!"),
+		Headers: map[string]string{
+			"FeatureFlag": strconv.FormatBool(featureFlag),
+		},
 		StatusCode: 200,
-	}, nil
+	}, err
 }
 
 func main() {
