cddc9f220319e2c0bcab1475ff5794d11bb8c96a
Casey Lee <casey.lee@stelligent.com>
2018-11-05 10:54:57 -0800

    update service name from cfn to cloudformation

--- toolchain/check-drift.sh
+++ toolchain/check-drift.sh
@@ -14,23 +14,23 @@ STACK_NAME=$1
 aws cloudformation describe-stacks --stack-name ${STACK_NAME} > /dev/null 2>&1 || exit 0
 
 ### Initiate drift detection
-DRIFT_DETECTION_ID=$(aws cfn detect-stack-drift --stack-name ${STACK_NAME} --query StackDriftDetectionId --output text)
+DRIFT_DETECTION_ID=$(aws cloudformation detect-stack-drift --stack-name ${STACK_NAME} --query StackDriftDetectionId --output text)
 
 ### Wait for detection to complete
 echo -n "Waiting for drift detection to complete..."
 while true; do
-    DETECTION_STATUS=$(aws cfn describe-stack-drift-detection-status --stack-drift-detection-id ${DRIFT_DETECTION_ID} --query DetectionStatus --output text) 
+    DETECTION_STATUS=$(aws cloudformation describe-stack-drift-detection-status --stack-drift-detection-id ${DRIFT_DETECTION_ID} --query DetectionStatus --output text) 
     if [ "DETECTION_IN_PROGRESS" = "${DETECTION_STATUS}" ]; then 
         echo -n "."
         sleep 1 
     elif [ "DETECTION_FAILED" = "${DETECTION_STATUS}" ]; then 
-        DETECTION_STATUS_REASON=$(aws cfn describe-stack-drift-detection-status --stack-drift-detection-id ${DRIFT_DETECTION_ID} --query DetectionStatusReason --output text)
-        STACK_DRIFT_STATUS=$(aws cfn describe-stack-drift-detection-status --stack-drift-detection-id ${DRIFT_DETECTION_ID} --query StackDriftStatus --output text) 
+        DETECTION_STATUS_REASON=$(aws cloudformation describe-stack-drift-detection-status --stack-drift-detection-id ${DRIFT_DETECTION_ID} --query DetectionStatusReason --output text)
+        STACK_DRIFT_STATUS=$(aws cloudformation describe-stack-drift-detection-status --stack-drift-detection-id ${DRIFT_DETECTION_ID} --query StackDriftStatus --output text) 
         echo ${STACK_DRIFT_STATUS}
         echo "WARNING: ${DETECTION_STATUS_REASON}"
         break
     else
-        STACK_DRIFT_STATUS=$(aws cfn describe-stack-drift-detection-status --stack-drift-detection-id ${DRIFT_DETECTION_ID} --query StackDriftStatus --output text) 
+        STACK_DRIFT_STATUS=$(aws cloudformation describe-stack-drift-detection-status --stack-drift-detection-id ${DRIFT_DETECTION_ID} --query StackDriftStatus --output text) 
         echo ${STACK_DRIFT_STATUS}
         break
     fi
@@ -38,7 +38,7 @@ done
 
 ### Describe the drift details
 if [ "DRIFTED" = "${STACK_DRIFT_STATUS}" ]; then 
-    aws cfn describe-stack-resource-drifts \
+    aws cloudformation describe-stack-resource-drifts \
         --stack-name ${STACK_NAME} \
         --query 'StackResourceDrifts[?StackResourceDriftStatus!=`IN_SYNC`].{Type:ResourceType, Resource:LogicalResourceId, Status:StackResourceDriftStatus, Diff:PropertyDifferences}' >&2 
     exit 1 
